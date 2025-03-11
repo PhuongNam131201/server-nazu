10a2edd10c0bc951fbca4a6cb5f5c19f37e3330b
PhuongNam131201 <npnam13122001@gmail.com>
2025-03-11 22:41:53 +0700

    authRouter

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const authRouter = require('./routers/authRouter');
 app.use(cors());
-
+app.use(express.json());
 const PORT = 3001;
-app.get('/auth/hello', (req, res) => {
-    res.send('Hello World');
-})
+app.use('/auth',authRouter);
 app.listen(PORT, (err) => {
 	if (err) {
 		console.log(err);
